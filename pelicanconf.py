dbeb0011eaecd9cdecbe274c072b204f4a39a002
Shane Lin <ssl2ab@virginia.edu>
2018-03-07 13:05:36 -0500

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import io
+import json
+
+AUTHOR = ''
+SITENAME = 'New Salem - Pelican'
+SITEURL = ''
+THEME = "/Users/ssl2ab/projects/pelican-themes/pelican-twitchy"
+
+PATH = 'content'
+TAG_NAMES = json.load(open("tags.json"))
+CAT_NAMES = {"swp":"Salem Witchcraft Papers"}
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
