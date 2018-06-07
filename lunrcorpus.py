2d9a377b278ad9c98b3363f5a659c1b92a4b8d5b
Shane Lin <ssl2ab@virginia.edu>
2018-06-07 18:16:08 -0400

    Add per-document date parsing for search corpus generation

--- lunrcorpus.py
+++ lunrcorpus.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import json
 import markdown
 import re
+from dateutil.parser import parse
+import datetime
+import os
 
 pathlist = Path("content/").glob('**/*.md')
 corpus = []
@@ -21,17 +24,30 @@ for path in pathlist:
             if ":" in line:
                 metadata[line.split(":",1)[0].lower()] = line.split(":",1)[1].strip()
         md = page.read()
+        md = re.sub(
+            r'\<div\ markdown\ class=\"doc\"\ id=\"n[0-9]{1,3}\.[0-9]{1,3}\"\>', '', md)
         if len(md.split("\n\n# Document: "))>1:
             for doc in md.split("\n\n# Document: "):
-                doc_id = doc[:doc.find("\n\n")]
+                if len(doc.split())==0:
+                    continue
+                doc_id = doc[:doc.find("\n\n")].split()
+                date_match = re.search(
+                    r'\[\+*\?*\s*\b(January|February|March|April|May|June|July|August|September|October|November|December)\b\s*[\d]{0,2}\s*[\,]?\s*\d{4}\s*', doc)
+                if date_match:
+                    datestr = re.search(
+                        r'[a-zA-Z0-9 ,]{9,}', date_match.group()).group().strip()
+                    date = parse(
+                        datestr, default=datetime.datetime(1691, 1, 1))
                 doc_html = markdown.markdown(doc[doc.find("\n\n")+2:])
-                doc_text = ''.join(BeautifulSoup(doc_html).findAll(text=True)).replace("\n","").replace("\t","")
-                corpus.append({"id": doc_id,"slug": metadata["slug"],"title":metadata["title"],"content":doc_text})
+                doc_text = ''.join(BeautifulSoup(doc_html,"lxml").findAll(text=True)).replace("\n","").replace("\t","")
+                corpus.append(
+                    {"id": doc_id, "slug": metadata["slug"], "title": metadata["title"], "content": doc_text, "date": str(date)})
         else:
             html = markdown.markdown(md)
             text = ''.join(BeautifulSoup(html).findAll(text=True))
-            corpus.append({"id": metadata["slug"],"slug": metadata["slug"],"title":metadata["title"],"content":text})
-
+            corpus.append({"id": metadata["slug"], "slug": metadata["slug"], "title": metadata["title"], "date": metadata["date"], "content": text})
 print("Writing to corpus")
-with open("corpus.json", 'w') as cout:
+with open("./content/search/corpus.json", 'w') as cout:
     cout.write(json.dumps(corpus))
+print("Running index script")
+os.system("cat content/search/corpus.json | scripts/build-index > content/search/idx.json")
