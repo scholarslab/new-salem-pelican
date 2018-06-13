e2b83b1c72b6a6782beb3ba80a67e72581f4c479
Shane Lin <ssl2ab@virginia.edu>
2018-06-13 13:27:42 -0400

    Corrected date specificity, made date regexes more flexible to accomodate variety of input formats

--- lunrcorpus.py
+++ lunrcorpus.py
@@ -9,10 +9,12 @@ import os
 
 pathlist = Path("content/").glob('**/*.md')
 corpus = []
+wrong_dates = []
+no_dates = []
 for path in pathlist:
     # because path is object not string
     pathstr = str(path)[7:]
-    if "tag/" in pathstr:
+    if "tag/" in pathstr or "SalVRec" in pathstr:
         continue
     with open("content"+pathstr, 'r') as page:
         print("Parsing: "+pathstr)
@@ -26,22 +28,37 @@ for path in pathlist:
         md = page.read()
         md = re.sub(
             r'\<div\ markdown\ class=\"doc\"\ id=\"n[0-9]{1,3}\.[0-9]{1,3}\"\>', '', md)
-        if len(md.split("\n\n# Document: "))>1:
-            for doc in md.split("\n\n# Document: "):
-                if len(doc.split())==0:
+        if len(md.split("\n# Document: "))>1:
+            for doc in md.split("\n# Document: "):
+                if len(doc.strip().split("\n"))<=1:
+                    #Ignore single lines (incl. the first split single div line)
                     continue
-                doc_id = doc[:doc.find("\n\n")].split()
+                doc_id = doc[:doc.find("\n")].strip()
+                # regex full date match
                 date_match = re.search(
-                    r'\[\+*\?*\s*\b(January|February|March|April|May|June|July|August|September|October|November|December)\b\s*[\d]{0,2}\s*[\,]?\s*\d{4}\s*', doc)
+                    r'\[\+*\?*\=*\s*\b(January|Jan|February|Feb|March|Mar|April|Apr|May|June|July|August|Aug|September|Sept|October|Oct|November|Nov|December|Dec)\b\s*[\d]{1,2}\s*[\,\.]?\s*\d{4}\s*', doc)
                 if date_match:
-                    datestr = re.search(
-                        r'[a-zA-Z0-9 ,]{9,}', date_match.group()).group().strip()
-                    date = parse(
-                        datestr, default=datetime.datetime(1691, 1, 1))
+                    date = re.search(r'[a-zA-Z0-9 ,.]{9,}', date_match.group()).group().strip()
+                    datestr = parse(
+                        date, default=datetime.datetime(1691, 1, 1)).strftime("%Y-%m-%d")
+                else:
+                    #regex month only match
+                    month_match = re.search(
+                        r'\[\+*\?*\=*\s*\b(January|Jan|February|Feb|March|Mar|April|Apr|May|June|July|August|Aug|September|Sept|October|Oct|November|Nov|December|Dec)\b\s*[\,\.]?\s*\d{4}\s*', doc)
+                    if month_match:
+                        date = re.search(
+                            r'[a-zA-Z0-9 ,]{9,}', month_match.group()).group().strip()
+                        datestr = parse(
+                            date, default=datetime.datetime(1691, 1, 1)).strftime("%Y-%m")
+                    else:
+                        if re.search(r'\]\n', doc):
+                            wrong_dates.append(doc_id)
+                        else:
+                            no_dates.append(doc_id)
                 doc_html = markdown.markdown(doc[doc.find("\n\n")+2:])
                 doc_text = ''.join(BeautifulSoup(doc_html,"lxml").findAll(text=True)).replace("\n","").replace("\t","")
                 corpus.append(
-                    {"id": doc_id, "slug": metadata["slug"], "title": metadata["title"], "content": doc_text, "date": str(date)})
+                    {"id": doc_id, "slug": metadata["slug"], "title": metadata["title"], "content": doc_text, "date": datestr})
         else:
             html = markdown.markdown(md)
             text = ''.join(BeautifulSoup(html).findAll(text=True))
@@ -51,3 +68,9 @@ with open("./content/search/corpus.json", 'w') as cout:
     cout.write(json.dumps(corpus))
 print("Running index script")
 os.system("cat content/search/corpus.json | scripts/build-index > content/search/idx.json")
+print("\n\n###### SWP documents with suspected wrong formating #######\n\n")
+for doc in wrong_dates:
+    print(doc)
+print("\n\n###### SWP documents with no canonical date found #######\n\n")
+for doc in no_dates:
+    print(doc)
