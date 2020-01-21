abb81d3399fccef15e0897f9c4c810a306e995bd
Shane Lin <ssl2ab@virginia.edu>
2020-01-21 13:27:43 -0500

    bug fixes for tag checker

--- tag_checker.py
+++ tag_checker.py
@@ -27,17 +27,20 @@ for path in pathlist:
             for match in re.finditer(pattern, line):
                 person = match.group()[6:-6]
                 if person not in tags:
-                    print("Person not in tags for ", pathstr, ": ",person)
+                    print("Person not in tags for ", pathstr, ": ", person)
                     if pathstr in missing_tags:
-                        missing_tags[pathstr] = missing_tags[pathstr] + ", " + person
+                        if person not in missing_tags[pathstr]:
+                            missing_tags[pathstr].append(person)
                     else:
-                        missing_tags[pathstr] = ", "+person
+                        missing_tags[pathstr] = [person]
             line = page.readline()
 
 for path in missing_tags.keys():
     lines = open('content'+path).read().splitlines()
     if lines[4][:5] != "tags:":
-        print("Tags not on line 5!",path)
+        print("Tags not on line 5!", path)
         exit()
-    lines[4] = lines[4] + missing_tags[path]
+    print(path)
+    print("   "+", ".join(missing_tags[path]))
+    lines[4] = lines[4] + ", " + ", ".join(missing_tags[path])
     open('content'+path, 'w').write('\n'.join(lines))
